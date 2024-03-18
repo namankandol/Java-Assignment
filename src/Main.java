c4a28d019d21ccce1f8e4fe4a30f99eb55f1e99c
namankandol <82331362+namankandol@users.noreply.github.com>
2024-03-18 23:34:02 +0530

    completed assignment

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 enum Department {
     DEV, QA, SUPPORT
@@ -96,7 +100,31 @@ public class Main {
             LocalDate randomDate = LocalDate.of(2010 + random.nextInt(14), random.nextInt(12)+1, random.nextInt(28)+1);
             Department randomDepartment = Department.values()[random.nextInt(Department.values().length)];
             EAddress randomAddress = new EAddress(i%23==0?"Gurgaon":"City"+random.nextInt(10), String.valueOf(100000 + random.nextInt(900000)));
+
+            employees.add(new Employee(i, "Employee" + i, randomDate, randomDepartment, randomAddress));
         }
 
+        List<Employee> employeesJoinedBeforeJan2022 = employees.stream()
+                .filter(employee -> employee.getDateOfJoining().isBefore(LocalDate.of(2022, 1, 1)))
+                .toList();
+
+        List<Employee> employeesInGurgaonAndDev = employees.stream()
+                .filter(employee -> employee.geteAddress().getCity().equalsIgnoreCase("Gurgaon") && employee.getDepartment() == Department.DEV)
+                .toList();
+
+        Consumer<Employee> employeeConsumer = employee -> System.out.println(employee.getName());
+        employees.forEach(employeeConsumer);
+
+        employees.addAll(employees.subList(0, 100));
+
+        ConcurrentMap<Integer, Employee> employeeMap = employees.stream()
+                .collect(Collectors.toConcurrentMap(Employee::getId, emp -> emp, (existing, replacement) -> existing));
+
+        employeeMap.forEach((key, value) -> System.out.println("ID: " + key + ", Employee: " + value.getName()));
+
+        Map<Department, Long> departmentEmployeeCount = employees.parallelStream()
+                .collect(Collectors.groupingByConcurrent(Employee::getDepartment, Collectors.counting()));
+
+        departmentEmployeeCount.forEach((department, count) -> System.out.println(department + ": " + count));
     }
 }
